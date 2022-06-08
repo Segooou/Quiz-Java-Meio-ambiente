47e1801b023027a35ad4c995a82c2fe16ca6b919
Segooou <gimbutis23@gmail.com>
2022-06-08 15:28:21 -0300

    Commit

---
+++ src/Dentes.java
@@ -0,0 +1,2 @@
+public class Dentes extends Consumo{@Override public int calcular(int min){ 
+setTotalD(min*2); return getTotalD(); }public Dentes(){}}
---
+++ src/Lavar.java
@@ -0,0 +1,2 @@
+public class Lavar extends Consumo{@Override public int calcular(int min){
+setTotalL(min*7);return getTotalL();}public Lavar(){}}
